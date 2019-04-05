878f9854f1f94b3f2a056cf7d5e1d6c4c5fa689b
kkorona <high1uck@pusan.ac.kr>
2019-04-05 19:05:23 +0900

    메소드별 기능 분리, 생성자 작성, 알고리즘 분류와 제출 정보 긁어오기 작성

--- src/boj/gg/BaekjoonCrawler.java
+++ src/boj/gg/BaekjoonCrawler.java
@@ -13,61 +13,134 @@ import org.jsoup.select.Elements;
 
 public class BaekjoonCrawler {
 	
-	static final String MAINURL = "http://www.acmicpc.net/";
+	private static final String MAINURL = "http://www.acmicpc.net/";
+	private static final boolean SHOW_LOG = true;
+	private static final String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36";
 	
-	public static ArrayList<String> crawlProblemNumbers() {
-		Document doc = null;
-		
-		
-		String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36";
+	public Document problemPageDocument = null;
+	private Map<String,String> loginCookie = null;
+	
+	// Constructor
+	
+	public BaekjoonCrawler(String userID, String userPassword) {
+		checkInternetConnection();
+		acquireLoginCookie(userID,userPassword);
+	}
+	
+	// Methods
+	
+	public static void checkInternetConnection() {
+		Document document = null;
+		try {
+			document = Jsoup.connect(MAINURL)
+							.get();
+		} catch(IOException e) {
+			System.err.println("Unable to connect.");
+		}
+		if(document != null) {
+			System.err.println("Successfully connected to " + MAINURL);
+		}
+		else {
+			System.err.println("Failed to receive main page document.");
+		}
+	}
+	
+	public void acquireLoginCookie(String userID, String userPassword) {
 
 		// 전송할 폼 데이터
 		Map<String, String> data = new HashMap<>();
-		data.put("login_user_id", "아이디");
-		data.put("login_password", "비밀번호");
+		data.put("login_user_id", userID);
+		data.put("login_password", userPassword);
 		data.put("auto_login", "0");
-
+		
 		// 로그인(POST)
 		Connection.Response response = null;
 		try {
 		response = Jsoup.connect("https://www.acmicpc.net/signin")
-		                                    .userAgent(userAgent)
-		                                    .timeout(3000)
-		                                    .data(data)
-		                                    .method(Connection.Method.POST)
-		                                    .execute();
+                        .userAgent(userAgent)
+                        .timeout(3000)
+                        .data(data)
+                        .method(Connection.Method.POST)
+                        .execute();
 		} catch (IOException e) {
-			System.err.println("Fail to have cookie");
+			System.err.println("Failed to connect login server.");
 		}
 		
-		// 로그인 성공 후 얻은 쿠키.
+		// 로그인 성공 후 얻은 쿠키를 멤버 변수로 저장.
 		// 쿠키 중 TSESSION 이라는 값을 확인할 수 있다.
-		Map<String, String> loginCookie = response.cookies();
-		Document adminPageDocument = null;
-		try {
-		adminPageDocument = Jsoup.connect("https://www.acmicpc.net/problem/1000")
-                .userAgent(userAgent)
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
-                .header("Upgrade-Insecure-Requests", "1")	
-                .cookies(loginCookie) // 위에서 얻은 '로그인 된' 쿠키
-                .get();
-		} catch(IOException e) {
-			System.err.println("fail to crawProblem page");
+		loginCookie = response.cookies();
+		
+	}
+	
+	public void receiveProblemDocument(int problemID) {
+		Document document = null;
+		if(loginCookie == null) {
+			System.err.println("Login cookie is not acquired.");
+		}
+		else {
+			try {
+				document = Jsoup.connect("https://www.acmicpc.net/problem/1000")
+				                .userAgent(userAgent)
+				                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
+				                .header("Upgrade-Insecure-Requests", "1")	
+				                .cookies(loginCookie) // acquireLoginCookie에서 얻은 '로그인 된' 쿠키
+				                .get();
+			} catch(IOException e) {
+				System.err.println("Failed to crawl problem page");
+			}
+		}
+		problemPageDocument = document;
+	}
+	
+	public ArrayList<String> crawlAlgorithms() {
+		ArrayList<String> algorithms = null;
+		final String TARGET_CSS = "a.spoiler-link";
+		if(problemPageDocument == null) {
+			System.err.println("Problem page document is empty.");
+		}
+		else {
+			algorithms = new ArrayList<String>();
+			Elements algorithmTagList = problemPageDocument.select(TARGET_CSS);
+			for(Element algorithm : algorithmTagList) {
+				if(SHOW_LOG) {
+					System.err.println(algorithm.text());
+				}
+				algorithms.add(algorithm.text());
+			}
 		}
 		
-		
-		System.err.println(adminPageDocument);
+		return algorithms;
+	}
+	
+	public Map<String, String> crawlProblemSubmitState() {
+		Map<String, String> problemState = null;
+		final String TARGET_CSS = "table#problem-info";
+		final int CATEGORY_LIMIT = 6;
+		if(problemPageDocument == null) {
+			System.err.println("Problem page document is empty.");
+		}
+		else {
+			problemState = new HashMap<String, String>();
+			Element table = problemPageDocument.select(TARGET_CSS).get(0);
+			Elements categories = table.select("thead").get(0).select("tr").get(0).select("th");
+			Elements values = table.select("tbody").get(0).select("tr").get(0).select("td");
+			for(int i=0; i<CATEGORY_LIMIT; i++) {
+				String category = categories.get(i).text();
+				String value = values.get(i).text();
+				problemState.put(category,value);
+				if(SHOW_LOG) {
+					System.err.println(category + ": " + value);
+				}
+			}
 
-		
+		}
+		return problemState;
+	}
+	
+	public static ArrayList<String> crawlProblemNumbers() {
+		Document doc = null;		
 
 		ArrayList<String> problemIDList = new ArrayList<>();
-		try {
-		 doc = Jsoup.connect(MAINURL).get();
-		} catch(IOException e) {
-			System.err.println("Unable to connect.");
-		}
-		String title = doc.title();
-		System.err.println("Successfully connected to " + title);
 		
 		for(int pageNumber = 1;; pageNumber++) {
 			String problemPageURL = MAINURL + "/problemset/" + String.valueOf(pageNumber);
@@ -76,24 +149,30 @@ public class BaekjoonCrawler {
 			} catch(IOException e) {
 				break;
 			}
-			String targetCSS = "td.list_problem_id";
-			Elements currentProblemIDList = doc.select(targetCSS);
+			final String TARGET_CSS = "td.list_problem_id";
+			Elements currentProblemIDList = doc.select(TARGET_CSS);
 			
-			System.err.println("Page number " + pageNumber);
+			if(SHOW_LOG) {
+				System.err.println("Page number " + pageNumber);
+			}
 			if(currentProblemIDList.isEmpty()) {
 				System.err.println("Problem Number Crawling Finished.");
 				break;
 			}
 			for(Element problemID : currentProblemIDList) {
-				System.err.println(problemID.text());
+				if(SHOW_LOG) {
+					System.err.println(problemID.text());
+				}
 			}
 		}
 		return problemIDList;
 	}
 	
 	public static void main(String[] args) {			
-		ArrayList myProblemList = new ArrayList();
-		myProblemList = crawlProblemNumbers();
+		BaekjoonCrawler bojcrawl = new BaekjoonCrawler("아이디","비밀번호");
+		bojcrawl.receiveProblemDocument(1000);
+		Map <String,String> problemState = bojcrawl.crawlProblemSubmitState();
+		ArrayList<String> problemAlgorithms = bojcrawl.crawlAlgorithms();
 	}
 
 }
