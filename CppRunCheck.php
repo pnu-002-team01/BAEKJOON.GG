7e71026caf7f6ef123950ed156e1c797af1f2ab8
amwogns <amwogns2@naver.com>
2019-04-06 13:28:26 +0900

    코드 수정
    1. 쓰이지 않는 변수가 선언되어 있었음($value) -> delete

--- CppRunCheck.php
+++ CppRunCheck.php
@@ -5,7 +5,6 @@ $txt = $_POST[content];
 fwrite($myfile,$txt);
 fclose($myfile);
 $output;
-$return_var;
 $source=$_POST[source];
 $output=shell_exec("cppcheck " .$source . " 2>&1");
 echo "<pre>$output</pre>";
